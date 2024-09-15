c910025af2de1168d598ee082fbcfad188577c1a
Gagan Basnet <chatwidmeah@gmail.com>
2024-09-16 01:15:14 +0545

    implemented ngOndestroy for component cleanup

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,15 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
 
   constructor() {
 
   }
 
   ngOnInit() {
-    setInterval(() => {
+    console.log('ON INIT');
+    this.interval = setInterval(() => {
       const rnd = Math.random(); // 0 - 0.999999999999
 
       if (rnd < 0.5) {
@@ -26,6 +28,15 @@ export class ServerStatusComponent {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval)
   }
 
 }
