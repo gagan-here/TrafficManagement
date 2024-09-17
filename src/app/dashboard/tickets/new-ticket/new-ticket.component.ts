476098480f249258f22061d39d54bf3199f3f442
Gagan Basnet <chatwidmeah@gmail.com>
2024-09-17 10:46:54 +0545

    added code to handle formm submissions

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButonComponent } from '../../../shared/buton/buton.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, ButonComponent],
+  imports: [ControlComponent, ButonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  onSubmit(titleElement: HTMLInputElement) {
+    const enteredTitle = titleElement.value;
+    console.log('ENTERED TITLE: ' + enteredTitle);
+  }
 
 }
