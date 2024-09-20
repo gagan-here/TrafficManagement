0d199261188ff2850e03b7c7ee97a0451ecf1680
Gagan Basnet <chatwidmeah@gmail.com>
2024-09-20 23:38:10 +0545

    used viewchild signal function instead of viewchild decorator

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButonComponent } from '../../../shared/buton/buton.component';
 import { FormsModule } from '@angular/forms';
@@ -11,12 +11,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: string, ticketText: string) {
     console.log(title);
     console.log(ticketText);
 
-    this.form?.nativeElement.reset()
+    this.form().nativeElement.reset()
   }
 }
